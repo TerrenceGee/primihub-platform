6e964495a359c9c4590c3662e02c053c2ba37d7a
terrence <terrence1337@126.com>
2024-09-29 17:06:01 +0800

    fix add rest template user-agent

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/RemoteClient.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/RemoteClient.java
@@ -49,6 +49,7 @@ public class RemoteClient {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 
         Map<String, Object> map = new HashMap<>();
         map.put(RemoteConstant.HEAD, resembleHeadMap());
